44931201bfbb9922337f0af7d4c191798c695279
Artemoskalenko <artem.moskalenko.00@gmail.com>
2022-06-19 08:20:20 +0300

    admin changes

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import News
 
+
 def index(request):
     news = News.objects.all()
-    res = '<h1>Список новостей</h1>'
-    for item in news:
-        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-    return HttpResponse(res)
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context)
